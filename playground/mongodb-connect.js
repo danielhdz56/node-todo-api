6bb6e80889fead66a87c914605e2f9bfdddc6616
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-23 13:03:57 -0500

    created own id in mongodb

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,6 @@
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient; //this allows you to connect with the mongodb server and issue commands to manipulate the database
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => { //if TodoApp is not created it will be created
     if (err) {
         return console.log('Unable to connect to MongoDB server');
     }
@@ -10,7 +10,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     text: 'Something to do',
     //     completed: false
 
-    // }, (err, result) => {
+    // }, (err, result) => { //this is the callback function
     //     if (err) {
     //         return console.log('Unable to insert todo', err);
     //     }
@@ -20,6 +20,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
     // Insert new doc into Users (name, age, location)
     db.collection('Users').insertOne({
+        _id: 123,
         name: 'Daniel',
         age: 23,
         location: 'Houston'
@@ -27,9 +28,10 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         if (err) {
             return console.log('Unable to insert user', err);
         }
-
         console.log(JSON.stringify(result.ops, undefined, 2));
     });
-
-    db.close();
+    db.close(); //this closes the connection with the mongodb server
 });
+
+//      cd mongo/bin/
+//      ./mongod --dbpath ~/mongo-data/
